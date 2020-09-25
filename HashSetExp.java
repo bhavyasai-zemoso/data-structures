0c73cab7a297cc17e369c5161eceeb596b056773
Bhavya Sai <bhavya.survepalli@zemosolabs.com>
2020-09-25 15:08:16 +0530

    data structure third commit

---
+++ HashSetExp.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HashSetExp {
+    public static void main(String args[]) {
+        HashSet < String > hs = new HashSet < String > ();
+        hs.add("Harry");
+        hs.add("Potter");
+        hs.add("Potter");
+        Iterator <String> itr = hs.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
